5178325836f92e30c16f7da9b626b0c16e535b2e
dilan <mfadilan999@gmail.com>
2026-06-02 08:07:57 +0700

    memperbaiki bug login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -54,7 +54,7 @@ class AuthController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return redirect('/login')
+        return redirect('/')
             ->with('success', 'Anda telah berhasil keluar sistem.');
     }
 }
